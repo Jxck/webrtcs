d6c1924d2611c7897073fe1e00d08d837e8497f6
Jxck <block.rxckin.beats@gmail.com>
2014-05-03 17:08:30 +0900

    complete

--- public/client.js
+++ public/client.js
@@ -30,15 +30,19 @@ var socket = io.connect();
 
 var requester = null;
 var responser = null;
+var localStream = null;
+var remoteStream = null;
 
 $(function() {
   var $localVideo = $('#local-video').get(0);
+  var $remoteVideo = $('#remote-video').get(0);
   var $sdp = $('#sdp');
   var $ice = $('#ice');
 
   // stop video
   $('#stopVideo').click(function() {
-    $localVideo.src = "";
+    $localVideo.src = '';
+    localStream.stop();
   });
 
   socket.on('offer', function(offer) {
@@ -46,11 +50,19 @@ $(function() {
     responser = new webkitRTCPeerConnection(config.responser);
     responser.setRemoteDescription(new RTCSessionDescription(offer));
 
+    // add stream
+    responser.addStream(localStream);
+    responser.addEventListener('addstream', function(e) {
+      $remoteVideo.src = window.webkitURL.createObjectURL(e.stream);
+    }, false);
+    responser.addEventListener('removestream', function(e) {
+      $remoteVideo.src = '';
+    }, false);
+
     // candidate
     responser.onicecandidate = function(ice) {
       if (ice.candidate) {
         $ice.text($ice.text() + '\n' + prittyice(ice.candidate));
-        console.log(ice.candidate);
         socket.emit('ice', ice.candidate);
       } else {
         $ice.text($ice.text() + '\n==END CANDIDATE==');
@@ -83,6 +95,15 @@ $(function() {
     // create peer requester
     requester = new webkitRTCPeerConnection(config.requester);
 
+    // add stream
+    requester.addStream(localStream);
+    requester.addEventListener('addstream', function(e) {
+      $remoteVideo.src = window.webkitURL.createObjectURL(e.stream);
+    }, false);
+    requester.addEventListener('removestream', function(e) {
+      $remoteVideo.src = '';
+    }, false);
+
     // candidate
     requester.onicecandidate = function(ice) {
       if (ice.candidate) {
@@ -101,24 +122,11 @@ $(function() {
     }, console.error, config.rtcoption);
   });
 
-
   // start video
-  // navigator.webkitGetUserMedia(config.media, function success(stream) {
-  //   $localVideo.src = window.webkitURL.createObjectURL(stream);
-  //   $localVideo.play();
-  //   $localVideo.volume = 0;
-  // }, console.error);
-
-
-  // // 送信
-  // $('#ok').on('click', function() { // クリックしたら
-  //   var message = $('#message').val(); // 中身を取って
-  //   socket.emit('message', message); // サーバに送信
-  // });
-
-  // // 受信
-  // socket.on('message', function(data) { // 受信したら
-  //   var $li = $('<li>', { text: data }); // li を作って
-  //   $('#messages').append($li); // ul に追加
-  // });
+  navigator.webkitGetUserMedia(config.media, function success(stream) {
+    localStream = stream;
+    $localVideo.src = window.webkitURL.createObjectURL(stream);
+    $localVideo.play();
+    $localVideo.volume = 0;
+  }, console.error);
 });
